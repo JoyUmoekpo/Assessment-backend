b6af9fd82ad4237166e4206438660bd69a5a536d
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-02 15:22:44 -0600

    Added postName post method

--- server/index.js
+++ server/index.js
@@ -7,10 +7,12 @@ app.use(cors());
 
 app.use(express.json());
 
-const { getCompliment, getFortune } = require('./controller')
+const { getCompliment, getFortune, postName } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 
 app.get("/api/fortune", getFortune);
+
+app.post("/api/name/", postName)
 
 app.listen(4000, () => console.log("Server running on 4000"));
