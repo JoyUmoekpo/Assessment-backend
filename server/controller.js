2576dabf14d311b6990794ac3e9ca117943204d2
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-02 14:38:39 -0600

    Created fortune controller

--- server/controller.js
+++ server/controller.js
@@ -5,9 +5,21 @@ module.exports = {
 
         // choose random compliment
         let randomIndex = Math.floor(Math.random() * compliments.length);
+        
         let randomCompliment = compliments[randomIndex];
 
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["You have fame and success in your future", "You will be financially stable in the near future", "You will have good luck in all personal matters", "Your dreams will soon be your reality", "You're in for good days soon"];
+
+        // chooses random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
     }
 
 }
