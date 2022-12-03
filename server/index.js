91aff96f77a70f050f5c481b368e20fccfe12fd1
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-03 00:10:26 -0600

    Created get method route

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ app.use(cors());
 
 app.use(express.json());
 
-const { getCompliment, getFortune, postName } = require('./controller')
+const { getCompliment, getFortune, getClassCompliments } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
-
 app.get("/api/fortune", getFortune);
+app.get("/api/compliments/classmates", getClassCompliments);
 
 app.listen(4000, () => console.log("Server running on 4000"));
